e987c97487676d4fa7430902bcf68398d84ce64c
Prathmesh Parit <90789154+prathmeshparit@users.noreply.github.com>
2023-11-11 21:33:54 +0530

    Add files via upload

---
+++ Set1.java
@@ -0,0 +1,14 @@
+
+//Example of SetIntrface in java Collection Framewrok
+import java.util.*;
+
+public class Set1 {
+    public static void main(String args[]) {
+        Set<String> data = new LinkedHashSet<String>();
+        data.add("Java ");
+        data.add("Set ");
+        data.add("Collection ");
+        data.add("Framework");
+        System.out.println(data);
+    }
+}
